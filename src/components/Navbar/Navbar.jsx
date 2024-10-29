22ae4114e8b1fe703dfc4585175b9205c5ebf5f4
Israel Akpadja <drimmaculate147@gmail.com>
2024-10-29 13:58:30 +0000

    added and finished navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import searchIcon from "../../assets/search_icon.svg";
+import bellIcon from "../../assets/bell_icon.svg";
+import profileIcon from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="logo" className="logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={searchIcon} alt="search" className="icons" />
+        <p>Children</p>
+        <img src={bellIcon} alt="bell_icon" className="bell_icon" />
+        <div className="navbar-profile">
+          <img src={profileIcon} alt="profile_icon" className="profile" />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
